895ab750cd4754d35a456873a38897591722841f
weiwu <574014131@qq.com>
2024-10-31 15:55:52 +0800

    [#dev] init activity

---
+++ Android-mnist/app/src/main/java/com/example/mnist/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.mnist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+}
