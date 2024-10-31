c30a1e01cf8b7c713ccc6489225fe9170630f909
weiwu <574014131@qq.com>
2024-10-31 16:13:01 +0800

    [#dev] init home fragment

--- Android-mnist/app/src/main/java/com/example/mnist/MainActivity.java
+++ Android-mnist/app/src/main/java/com/example/mnist/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.mnist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
-import com.idlefish.flutterboost.FlutterBoost;
-import com.idlefish.flutterboost.FlutterBoostRouteOptions;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,28 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            AssetManager assetManager = getAssets();
-            InputStream inputStream = assetManager.open("one.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            ImageView image = findViewById(R.id.image);
-            image.setImageBitmap(bitmap);
+        Fragment homeFragment = new HomeFragment();
 
-            image.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    FlutterBoostRouteOptions options = new FlutterBoostRouteOptions.Builder()
-                            .pageName("simplePage")
-                            .arguments(new HashMap<>())
-                            .requestCode(1111)
-                            .build();
-                    FlutterBoost.instance().open(options);
-                }
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
     }
 }
